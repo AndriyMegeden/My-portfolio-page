5bab6b019c26a40994ee109801443961c99ec79b
Andiy <andriemeheden@gmail.com>
2025-01-17 23:59:51 +0200

    add scrollsmother gsap

--- src/app/page/landing/landing.component.ts
+++ src/app/page/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { gsap } from 'gsap';
@@ -6,6 +6,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { FirstPartComponent } from '../../components/first-part/first-part.component';
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
 import { MyProjectsComponent } from 'src/app/components/my-projects/my-projects.component';
+import { isPlatformBrowser } from '@angular/common';
+
+
 @Component({
   selector: 'app-landing',
   standalone: true,
@@ -17,7 +20,24 @@ import { MyProjectsComponent } from 'src/app/components/my-projects/my-projects.
 export class LandingComponent implements OnInit {
 
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngOnInit(): void {
-    
+    // if (isPlatformBrowser(this.platformId)) {
+    //   gsap.registerPlugin(ScrollTrigger);
+
+    //   import('gsap-trial/ScrollSmoother').then(({ default: ScrollSmoother }) => {
+    //     gsap.registerPlugin(ScrollSmoother);
+
+    //     ScrollSmoother.create({
+    //       wrapper: '.wrapper',
+    //       content: '.content',
+    //       smooth: 1.2,
+    //       effects: true
+    //     });
+    //   }).catch((error) => {
+    //     console.error('Помилка при завантаженні ScrollSmoother:', error);
+    //   });
+    // }
   }
 }
