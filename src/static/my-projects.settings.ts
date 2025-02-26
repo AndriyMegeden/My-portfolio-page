29aa68188c2a0b6a36f902b88b5e71846c296518
Andiy <andriemeheden@gmail.com>
2025-02-26 13:18:21 +0200

    add user project

--- src/static/my-projects.settings.ts
+++ src/static/my-projects.settings.ts
@@ -8,20 +8,26 @@ export const myProjectSettings: ArraySlider = {
       link: 'https://influencer-frontend-seven.vercel.app/en/main/home',
     },
     {
-      slide: 'assets/slider/be-best.png',
-      title: 'Be Best',
-      link: 'https://be-best-project.vercel.app/',
-    },
-    {
       slide: 'assets/slider/films.png',
       title: 'Best Movie',
       link: 'https://best-movies-2024.vercel.app/',
     },
     {
-        slide: 'assets/slider/vorfahr.png',
-        title: 'Vorfahr',
-        subtitle: 'Only auth, footer, header',
-        link: 'https://vorfahrqr.de/login',
-      },
+      slide: 'assets/slider/vorfahr.png',
+      title: 'Vorfahr',
+      subtitle: 'Only auth, footer, header',
+      link: 'https://vorfahrqr.de/login',
+    },
+    {
+      slide: 'assets/slider/user.png',
+      title: 'User Admin',
+      subtitle: 'Email: andriy@gmail.com\n Password: 111111',
+      link: 'https://user-three-taupe.vercel.app/',
+    },
+    {
+      slide: 'assets/slider/be-best.png',
+      title: 'Be Best',
+      link: 'https://be-best-project.vercel.app/',
+    },
   ],
 };
