6ee1b11dc864868f0a4abdcea5dd8d6fb1c5bce5
Andiy <andriemeheden@gmail.com>
2025-03-14 14:32:29 +0200

    change subtitles

--- src/static/my-projects.settings.ts
+++ src/static/my-projects.settings.ts
@@ -11,26 +11,26 @@ export const myProjectSettings: ArraySlider = {
     {
       slide: 'assets/slider/vorfahr.png',
       title: 'Vorfahr',
-      subtitle: 'Only auth, footer, header',
+      subtitle: 'I worked on the layout and responsive design of the header, footer, and the registration and login page. I also integrated translation files.',
       link: 'https://vorfahrqr.de/login',
     },
     {
       slide: 'assets/slider/user.png',
       title: 'User Admin',
-      subtitle: 'Email: andriy@gmail.com\n Password: 111111',
+      subtitle: 'This is an admin panel for an internet provider. I developed the project structure and worked on the layout. I also fully integrated the admin panel with Firebase. Here, you can create, edit, and delete users, search for them by location, and view statistics on active and inactive users. Here are the login details for the admin panel: Email: andriy@gmail.com\n Password: 111111',
       link: 'https://user-three-taupe.vercel.app/',
     },
     {
       slide: 'assets/slider/films.png',
       title: 'Best Movie',
-      subtitle: 'Only auth, footer, header',
+      subtitle: 'This is my personal project where I learned GSAP animations. With this knowledge, I created this awesome portfolio.',
       link: 'https://best-movies-2024.vercel.app/',
     },
    
     {
       slide: 'assets/slider/be-best.png',
       title: 'Be Best',
-      subtitle: 'Only auth, footer, header',
+      subtitle: 'This is a mobile app for creating personal goals. I worked on the adaptive layout for all pages, developed the project structure, and implemented various logic. I also tested the app on an iPhone, integrated Apple authentication, and set up Apple payments. I handled most of the front-end tasks.',
       link: 'https://be-best-project.vercel.app/',
     },
   ],
