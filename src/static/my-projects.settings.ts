7f44c03b353e0221e008cb7701eff6d6471986b7
Andiy <andriemeheden@gmail.com>
2025-04-01 20:54:29 +0300

    add project

--- src/static/my-projects.settings.ts
+++ src/static/my-projects.settings.ts
@@ -15,24 +15,29 @@ export const myProjectSettings: ArraySlider = {
       link: 'https://vorfahrqr.de/login',
     },
     {
+      slide: 'assets/slider/be-best.png',
+      title: 'Be Best',
+      subtitle: 'projects.beBest',
+      
+      link: '/assets/videos/be-best-video.mp4',
+    },
+    {
       slide: 'assets/slider/user.png',
       title: 'User Admin',
       subtitle: 'projects.user',
       link: 'https://user-three-taupe.vercel.app/',
     },
     {
+      slide: 'assets/slider/shopify.png',
+      title: 'Shopify',
+      subtitle: 'projects.shopify',
+      link: 'https://shopify-front-mauve.vercel.app/products.html',
+    },
+    {
       slide: 'assets/slider/films.png',
       title: 'Best Movie',
       subtitle: 'projects.films',
       link: 'https://best-movies-2024.vercel.app/',
-    },
-   
-    {
-      slide: 'assets/slider/be-best.png',
-      title: 'Be Best',
-      subtitle: 'projects.beBest',
-      
-      link: '/assets/videos/be-best-video.mp4',
     },
   ],
 };
